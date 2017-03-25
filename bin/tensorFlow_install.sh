2285959467ee0de9b513684437d206bfe82df9ec
alladmin <alladmin@ubuntuMain3.0>
2017-03-25 21:39:08 +0900

    firstcommit

---
+++ bin/tensorFlow_install.sh
@@ -0,0 +1,20 @@
+mkdir tensorFlow_test
+cd tensorFlow_test
+python -v
+python -V
+pip
+python -m pip -V
+sudo apt-get install python-pip
+sudo pip install --upgrade virtualenv
+python -m pip -V
+virtualenv --system-site-packages ~/lib/tensorflow
+ls ~/lib/tensorflow
+cd ~/lib/tensorflow
+source bin/activate
+pip install --upgrade tensorflow
+python
+pwd
+cd ~/f/program/test/tensorFlow_test
+ls
+mkdir tutorials
+cd tutorials
