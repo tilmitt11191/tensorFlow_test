8ba2c8a8a5f0fadd909e3af03d3b2a536284dd6e
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-05-14 20:59:55 +0900

    2017年 5月 14日 日曜日 20:59:26 JST

--- wikitext/convert_model_to_mysql.py
+++ wikitext/convert_model_to_mysql.py
@@ -1,6 +1,6 @@
 import sys
 import os
-
+import time
 from gensim import models
 import numpy as np
 
@@ -12,12 +12,15 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../lib/db")
 from table_wikitext_nodes import Table_nodes
 
 model = models.Doc2Vec.load('wikitext_doc2vec.model')
+timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
 
-#keys = model.docvecs.doctags.keys()
-keys = ["Final Fantasy VIII", "Final Fantasy X", "Dragon Ball"]
+keys = model.docvecs.doctags.keys()
+times = 1
+#keys = ["Final Fantasy VIII", "Final Fantasy X", "Dragon Ball"]
 for key in keys:
-	print(key)
+	print("times[" + str(times) + "]/[" + str(len(keys)) +"] key: " + key)
 	np_array = np.array(model.docvecs[key])
 	np_str = ",".join(str(i) for i in np_array)
-	node = Table_nodes(title=key, vector=np_str)
+	node = Table_nodes(title=key.encode("utf-8"), vector=np_str, timestamp=timestamp)
 	node.insert()
+	times += 1
