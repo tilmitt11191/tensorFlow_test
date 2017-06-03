f6f756c3e1a19ea93e6ef53ed0a22b368e918bd5
tilmitt11191 <tilmitt11@yahoo.co.jp>
2017-06-03 20:25:28 +0900

    2017年 6月  3日 土曜日 20:24:55 JST

--- lib/learnings/test_cnn.py
+++ lib/learnings/test_cnn.py
@@ -7,7 +7,8 @@ import os
 import sys
 import time
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../../lib/utils")
+sys.path.append(os.path.dirname(
+	os.path.abspath(__file__)) + "/../../lib/utils")
 from conf import Conf
 from log import Log
 log = Log.getLogger()
@@ -18,7 +19,38 @@ from enwiki_nodes import Table_nodes
 from enwiki_operator import Mysql_operator
 
 import tensorflow as tf
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' # or any {'0', '1', '2'}
+from tensorflow.contrib.slim.python.slim.nets.inception_v3 import inception_v3_base
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}
+
+
+def parse_sequence_example(serialized, image_feature, caption_feature):
+	"""Parses a tensorflow.SequenceExample into an image and caption.
+
+	Args:
+	  serialized: A scalar string Tensor; a single serialized SequenceExample.
+	  image_feature: Name of SequenceExample context feature containing image
+		data.
+	  caption_feature: Name of SequenceExample feature list containing integer
+		captions.
+
+	Returns:
+	  encoded_image: A scalar string Tensor containing a JPEG encoded image.
+	  caption: A 1-D uint64 Tensor with dynamically specified length.
+	"""
+	print("image_feature: " + str(image_feature))
+	print("caption_feature: " + str(caption_feature))
+	context, sequence = tf.parse_single_sequence_example(
+		serialized,
+		context_features={
+			image_feature: tf.FixedLenFeature([], dtype=tf.string)
+		},
+		sequence_features={
+			caption_feature: tf.FixedLenSequenceFeature([], dtype=tf.int64),
+		})
+
+	encoded_image = context[image_feature]
+	caption = sequence[caption_feature]
+	return encoded_image, caption
 
 
 def main(argv=None):
@@ -28,7 +60,22 @@ def main(argv=None):
 	#records = db.session.query(Table_nodes).all()
 	records = db.session.query(Table_nodes).filter(Table_nodes.id < 10).all()
 	print(len(records))
-	print(records[0].title,records[0].sentence)
+	ids = []
+	titles = []
+	docs = []
+	sentences = []
+	for record in records:
+		ids.append(record.id)
+		titles.append(record.title)
+		docs.append(record.doc2vec)
+		sentences.append(record.sentence)
+
+	parse_sequence_example(docs[0], sentences[0], ids[0])
+
+	#scope = "InceptionV3"
+	#with tf.variable_scope(scope, "InceptionV3", [docs]) as scope:
+	#	net, end_points = inception_v3_base(docs, scope=scope)
+
 
 if __name__ == '__main__':
 	tf.app.run()
